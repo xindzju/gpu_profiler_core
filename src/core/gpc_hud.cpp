593956029e3bb52f3884752c0580d35c3e56b486
charles <stdeng@nvidia.com>
2021-07-12 18:01:04 +0800

    add hub.h & hub.cpp

--- src/core/gpc_hud.cpp
+++ src/core/gpc_hud.cpp
@@ -2,5 +2,32 @@
 
 namespace gpc {
 
+	void GPCHUD::OnInit() {
+		ImGui::Render();
+		return;
+	}
+
+	void GPCHUD::OnUpdate() {
+		ImGui::Render();
+		return;
+	}
+
+	void GPCHUD::OnRender() {
+		ImGui::Render();
+		return;
+	}
+
+	void GPCHUD::OnDestory() {
+		ImGui::Render();
+		return;
+	}
+
+	bool GPCHUDBackendDX12::Init() {}
+
+	void GPCHUDBackendDX12::ShutDown() {}
+
+	void GPCHUDBackendDX12::NewFrame() {}
+
+	void GPCHUDBackendDX12::RenderDrawData() {}
 }
 
--- src/core/gpc_hud.h
+++ src/core/gpc_hud.h
@@ -1,13 +1,60 @@
+/*
+* Integrated Dear ImGui: platform backend(Win32, SDL, glfw), render backend(dx12, vulkan, ogl)
+*/
 #pragma once
+#include "imgui.h"
+#include "gpc_utils.h"
 
 namespace gpc {
-	//frame inspector
-	class GPCHUD {
+	struct FrameResource {
 
 	};
 
-	//memory inspector
+	//frame inspector, memory inspector, process inspector(?)
+	class GPCHUD {
+	public:
+		GPCHUD() {
+			m_backend = std::make_unique<GPCHUDBackendDX12>();
+		};
+		~GPCHUD() {};
 
-	//process inspector
+		virtual void OnInit();
+		virtual void OnRender();
+		virtual void OnUpdate();
+		virtual void OnDestory();
 
+	private:
+		std::unique_ptr<FrameResource> m_frameResource;
+		std::unique_ptr<GPCHUDBackend> m_backend = nullptr;
+	};
+
+	class GPCHUDBackend {
+	public:
+		virtual bool Init() = 0;
+		virtual void ShutDown() = 0;
+		virtual void NewFrame() = 0;
+		virtual void RenderDrawData() = 0;
+	};
+
+	class GPCHUDBackendDX12 : public GPCHUDBackend {
+	public:
+		GPCHUDBackendDX12() {};
+		~GPCHUDBackendDX12() {};
+
+		//platform(win32), render(dx12)
+		virtual bool Init();
+		virtual void ShutDown();
+		virtual void NewFrame();
+		virtual void RenderDrawData();
+	};
+
+	/*
+	class GPCHUDBackendVK {
+		//platform(win32), render(dx12)
+		virtual bool Init();
+		virtual void ShutDown();
+		virtual void NewFrame();
+		virtual void RenderDrawData();
+	};
+	*/
 }
